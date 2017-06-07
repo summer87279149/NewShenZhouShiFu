905df8506f428a2966f23a958a2f7b125e4168c4
Admin <1033948449@qq.com>
2017-06-07 09:24:42 +0800

    Initial Commit

---
+++ NewShenZhouShiFu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewShenZhouShiFu
+//
+//  Created by Admin on 2017/6/7.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NewShenZhouShiFu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NewShenZhouShiFu
+//
+//  Created by Admin on 2017/6/7.
+//  Copyright © 2017年 Summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
